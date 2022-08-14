607385139b3f9b670c223c04159dbf61276ba90a
Juan Escandón <juanescalo02@gmail.com>
2022-08-13 21:49:38 -0500

    Se agrega archivo del segundo desafío de la clase 8

--- natural_nums_v2.py
+++ natural_nums_v2.py
@@ -1,10 +1,16 @@
 def squareNaturalNums(iter):
     list = []
+    Divisive = []
     
     for i in range(1, iter+1):
-        list.append(i**2)
+        mod = i % 3
+        if mod == 0:
+            Divisive.append(i)
+        else:
+            list.append(i**2)
 
     print("Lista de numeros naturales al cuadrado", list)
+    print("Los cuadrados excluidos fueron: ", Divisive)
 
 
 def run():
