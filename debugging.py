e088ba8345b28cf1dac4644b93969ac1f07b7628
Juan Escandón <juanescalo02@gmail.com>
2022-08-14 21:14:50 -0500

    Se agregan modificaciones para manejo de excepciones

--- debugging.py
+++ debugging.py
@@ -1,12 +1,21 @@
 def divisors(num):
-    divisors = [i for i in range(1, num + 1) if num % i == 0]
-    return divisors
+    try:
+        if num < 0:
+            raise ValueError("No puedes ingresar un número negativo")
+        divisors = [i for i in range(1, num + 1) if num % i == 0]
+        return divisors
+    except ValueError as ve:
+        print(ve)
+        return False
 
 
 def run():
-    num = int(input("Ingresa un número: "))
-    print(divisors(num))
-    print("Terminó mi programa")
+    try:
+        num = int(input("Ingresa un número: "))
+        print(divisors(num))
+        print("Terminó mi programa")
+    except ValueError:
+        print("Debes ingresar un número")
 
 
 if __name__ == "__main__":
